3a18c0b7aeabe6e8b842841a18fa26fdc1e3a0d0
charanjattana648 <singhc5@douglascollege.ca>
2019-11-06 13:21:30 -0800

    CruiseRVListActivity.java is added to show list of cruise in recycler view

--- app/src/main/java/com/example/cruiseapp/CruiseRVListActivity.java
+++ app/src/main/java/com/example/cruiseapp/CruiseRVListActivity.java
@@ -1,13 +1,104 @@
 package com.example.cruiseapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-public class CruiseRVListActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+public class CruiseRVListActivity extends AppCompatActivity implements OnEventListener<String> {
+
+    private RecyclerView recyclerView;
+    private CustomAdapter customAdapter=new CustomAdapter();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cruise_rvlist);
+
+        recyclerView=findViewById(R.id.list_recycleView);
+        Intent intent=getIntent();
+        ArrayList<Cruise> cruise_arrayList=new ArrayList<>();
+        if(intent!=null)
+        {
+            cruise_arrayList=getCruiseList(intent.getStringExtra("cruiseData"));
+            customAdapter=new CustomAdapter(cruise_arrayList);
+            recyclerView.setAdapter(customAdapter);
+
+            LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+
+            recyclerView.setLayoutManager(linearLayoutManager);
+
+        }
+
+
+        //adapter
+
+
+
+    }
+    public String[] jsonArraytoStringArray(JSONArray jsonArray,int length)
+    {
+        String[] simple_array=new String[length];
+        for(int i=0;i<length;i++)
+        {
+            try {
+                simple_array[i]=jsonArray.get(i).toString();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return simple_array;
+    }
+    public ArrayList<Cruise> getCruiseList(String result)
+    {
+        JSONArray cruiseArray=null;
+        JSONObject cruiseObject=null;
+        ArrayList<Cruise> cList=new ArrayList<>();
+
+        try {
+            cruiseArray=new JSONArray(result);
+            String[] departureArray;
+            String[] destinationArray;
+            for(int i=0;i<cruiseArray.length();i++)
+            {
+                Cruise c=new Cruise();
+                cruiseObject = cruiseArray.getJSONObject(i);
+                c.setCruiseName(cruiseObject.getString("CruiseName"));
+                c.setCompanyName(cruiseObject.getString("CompanyName"));
+                c.setCrew(cruiseObject.getInt("Crew"));
+                c.setGuests(cruiseObject.getInt("Guests"));
+                c.setCruiseImage(cruiseObject.getString("CruiseImage"));
+                c.setLaunched(cruiseObject.getInt("Launched"));
+                JSONArray jsonArray=cruiseObject.getJSONArray("Departs_from");
+                departureArray=jsonArraytoStringArray(jsonArray,jsonArray.length());
+
+                jsonArray=cruiseObject.getJSONArray("Sails_to");
+                destinationArray=jsonArraytoStringArray(jsonArray,jsonArray.length());
+                c.setDeparts_from(departureArray);
+                c.setSails_to(destinationArray);
+                cList.add(c);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return cList;
+    }
+
+    @Override
+    public void onSuccess(String result) {
+
+    }
+
+    @Override
+    public void onFailure(String result) {
+
     }
 }
