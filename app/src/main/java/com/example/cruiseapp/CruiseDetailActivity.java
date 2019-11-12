d4ea370b294d742eb8b777dcffe31b53143a179b
charanjattana648 <singhc5@douglascollege.ca>
2019-11-12 00:25:57 -0800

    AndroidManifest.xml modified

---
+++ app/src/main/java/com/example/cruiseapp/CruiseDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.cruiseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CruiseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cruise_detail);
+    }
+}
