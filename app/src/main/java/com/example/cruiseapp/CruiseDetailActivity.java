03ca0da6754cd927e06435a5ab807f1058b5e474
charanjattana648 <singhc5@douglascollege.ca>
2019-11-12 00:26:44 -0800

    CruiseDetailActivity.java added

--- app/src/main/java/com/example/cruiseapp/CruiseDetailActivity.java
+++ app/src/main/java/com/example/cruiseapp/CruiseDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.cruiseapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class CruiseDetailActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class CruiseDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cruise_detail);
+        Intent intent=getIntent();
+        if(intent!=null)
+        {
+            Toast.makeText(this, ""+intent.getStringExtra("name"), Toast.LENGTH_SHORT).show();
+            Log.d("name cdactivity", "onCreate: "+intent.getStringExtra("name"));
+        }
     }
 }
